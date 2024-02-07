34e22e20e9d9162b4f501890818cf832ffac20a2
Hadeyfisayoh <73082594+Hadeyfisayoh@users.noreply.github.com>
2024-02-07 15:42:50 +0100

    fxf
     v

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,14 @@
+describe("Test GET /launches", () => {
+  test("it should  respond with 200 sucess", () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe("Test POST /launches", () => {
+  test("It should respond with 200 success", () => {});
+
+  test("It should catch missing required properties", () => {});
+
+  test("It should catch invalid dates", () => {});
+});
